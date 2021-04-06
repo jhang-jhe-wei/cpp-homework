c616cd02ad305aec8b365eb166c2a179cefc009b
wells <59594475+jhang-jhe-wei@users.noreply.github.com>
2021-04-06 17:38:52 +0800

    Update VecNf.h

--- 6.2/VecNf.h
+++ 6.2/VecNf.h
@@ -1,17 +1,25 @@
 #pragma once
 #include <iostream>
+#include <vector>
 using namespace std;
-class VecNf
+
+class VecNf 
 {
-public:	
-	VecNf(float* s = nullptr, int n = 0);
+public:
+	float* vStart;
+	int count;
+	
+	VecNf();
+	VecNf(float* dest, int cnt);
+	VecNf(const VecNf& rhs);
+
 	int Size();
-	float* getElement();
-	float operator[](int index);		
-	void operator =(VecNf v);
-private:
-	int size;
-	float* element;
+	float operator[](int i);
+	VecNf operator+(VecNf v);
+	VecNf operator-(VecNf v);
+	float operator*(VecNf v);
+	friend VecNf operator*(float multi, VecNf v);
+	friend VecNf operator*(VecNf v, float multi);
+	void operator=(VecNf v);
+	friend ostream& operator<<(ostream& out, VecNf v);
 };
-
-
