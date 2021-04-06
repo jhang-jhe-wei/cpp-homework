54b7af5167333e85709b344062cb24daf845d153
wells <59594475+jhang-jhe-wei@users.noreply.github.com>
2021-04-06 17:39:07 +0800

    Update VecNf.cpp

--- 6.2/VecNf.cpp
+++ 6.2/VecNf.cpp
@@ -1,30 +1,124 @@
 #include "VecNf.h"
-VecNf::VecNf(float * s, int n)
+
+VecNf::VecNf()
 {
-	element = s;
-	size = n;
+	vStart = nullptr;
+	count = 0;
 }
+
+VecNf::VecNf(float *dest, int cnt)
+{
+	vStart = dest;
+	count = cnt;
+}
+
+VecNf::VecNf(const VecNf& rhs){
+	vStart = rhs.vStart;
+	count = rhs.count;
+}
+//VecNf(const VecNf& rhs);
 
 int VecNf::Size()
 {
-	return size;
+	return count;
 }
 
-float * VecNf::getElement()
+float VecNf::operator[](int i)
 {
-	return element;
+	return vStart[i];
 }
 
-float VecNf::operator[](int index)
+VecNf VecNf::operator+(VecNf v)
 {
-	if (index > size - 1 || index < 0)
+	if (count != v.count)
 	{
-		cout << "out of range!" << endl;
+		cout << "dimensions inconsistent" << endl;
+		return VecNf();
+	}
+	else
+	{
+		float *temp;
+		temp = new float[count];
+		for (int i = 0; i <= count; i++)
+		{
+			temp[i] = vStart[i] + v.vStart[i];
+		}
+		return VecNf(temp, count);
+	}
+}
+
+VecNf VecNf::operator-(VecNf v)
+{
+	if (count != v.count)
+	{
+		cout << "dimensions inconsistent" << endl;
+		return VecNf();
+	}
+	else
+	{
+		float *temp;
+		temp = new float[count];
+		for (int i = 0; i <= count; i++)
+		{
+			temp[i] = vStart[i] - v.vStart[i];
+		}
+		return VecNf(temp, count);
+	}
+}
+
+float VecNf::operator*(VecNf v)
+{
+	if (count != v.count)
+	{
+		cout << "dimensions inconsistent" << endl;
 		return 0;
 	}
-	else 
+	else
 	{
-		return element[index];
-	}	
+		float sum = 0;
+		for (int i = 0; i < count; i++)
+		{
+			sum += (vStart[i] * v.vStart[i]);
+		}
+		return sum;
+	}
 }
 
+VecNf operator*(float multi, VecNf v)
+{
+	float *temp;
+	temp = new float[v.count];
+	for (int i = 0; i < v.count; i++)
+	{
+		temp[i] = multi * v.vStart[i];
+	}
+	return VecNf(temp, v.count);
+}
+
+VecNf operator*(VecNf v, float multi)
+{
+	float *temp;
+	temp = new float[v.count];
+	for (int i = 0; i < v.count; i++)
+	{
+		temp[i] = v.vStart[i] * multi;
+	}
+	return VecNf(temp, v.count);
+}
+
+void VecNf::operator=(VecNf v)
+{
+	cout << "ASSIGNMENT!!!" << endl;
+	count = v.count;
+	vStart = v.vStart;
+}
+
+ostream &operator<<(ostream &out, VecNf v)
+{
+	for (int i = 0; i < v.count; i++)
+	{
+		out << v[i];
+	}
+	cout << endl;
+	return out;
+}
